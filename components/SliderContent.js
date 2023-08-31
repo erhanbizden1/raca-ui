8c3cd3c1df6c3da316ecd2495e7a31c81743d64f
Erhan Bizden <erhanb@dopinghafiza.com>
2023-08-31 20:44:41 +0300

    new-fixed

--- components/SliderContent.js
+++ components/SliderContent.js
@@ -5,52 +5,63 @@ import "swiper/css/pagination";
 import { Pagination, Navigation } from "swiper";
 import Image from "next/image";
 import Link from "next/link";
-export default function SliderContent({ sliderContent,color }) {
+import Head from "next/head";
+export default function SliderContent({ sliderContent, color }) {
     return (
-        <div className="container mb-[100px]">
-            <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 lg:mt-[70px] lg:pb-[100px] ">
-                <div className="order-last xl:order-first">
-                    <h1 className={`text-[36px] md:text-[64px]  md:leading-[80px] lg:pl-0 lg:text-[88px] font-merriweather leading-[54px] lg:leading-[111px]`} style={{color:color ? color : "#000C1F"}}>{sliderContent.title}</h1>
-                    <div>
-                        <div className={` font-[500] text-lg`} dangerouslySetInnerHTML={{ __html: sliderContent.description }} style={{color:color ? color : "#000C1F",opacity:color ? "opacity-80":"opacity-100"}}></div>
+        <>
+            <Head>
+                <title>Royal Automobile Club of Australia - {sliderContent.title}</title>
+            </Head>
+            <div className="container mb-[100px]">
+                <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 lg:mt-[70px] lg:pb-[100px] ">
+                    <div className="order-last xl:order-first">
+                        <h1 className={`text-[36px] md:text-[64px]  md:leading-[80px] lg:pl-0 lg:text-[88px] font-merriweather leading-[54px] lg:leading-[111px]`} style={{ color: color ? color : "#000C1F" }}>{sliderContent.title}</h1>
+                        <div>
+                            <div className={` font-[500] text-lg`} dangerouslySetInnerHTML={{ __html: sliderContent.description }} style={{ color: color ? color : "#000C1F", opacity: color ? "opacity-80" : "opacity-100" }}></div>
+                        </div>
+                        {
+                            sliderContent?.buttonLink && sliderContent?.buttonText ?
+                                <Link href={sliderContent.buttonLink}>
+                                    <button className="px-[73px] bg-blue xl:hover:bg-[#002966] transition-all ease-in-out border-[2px] border-black py-[15px] mt-[36px] relative min-w-[268px] text-white">
+                                        <span className="text-lg transition ease-in-out">{sliderContent.buttonText}</span>
+                                    </button>
+                                </Link>
+                                : ""
+                        }
                     </div>
-                    {
-                        sliderContent?.buttonLink && sliderContent?.buttonText ?
-                            <Link href={sliderContent.buttonLink}>
-                                <button className="px-[73px] bg-blue xl:hover:bg-[#002966] transition-all ease-in-out border-[2px] border-black py-[15px] mt-[36px] relative min-w-[268px] text-white">
-                                    <span className="text-lg transition ease-in-out">{sliderContent.buttonText}</span>
-                                </button>
-                            </Link>
-                            : ""
-                    }
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={1}
+                        navigation={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Navigation, Pagination]}
+                        className="mySwiper swiperCustom"
+                    >
+                        {
+                            sliderContent.slider.map((sliderItem) => {
+                                return (
+                                    <SwiperSlide key={sliderItem.id} className="!flex items-start pl-[10px]"><div className="relative w-full">
+                                        {
+                                            sliderItem.thumbnail.data ?
+                                                <>
+                                                    <div className="border border-border absolute left-0 top-0 w-full h-full scale-[1.03] scale-y-[1.05]"></div>
+                                                    <Image src={sliderItem.thumbnail.data[0].attributes.url} width={13} layout="responsive" height={13} alt=""></Image>
+                                                </>
+                                                :
+                                                ""
+                                        }
+
+
+                                    </div></SwiperSlide>
+                                )
+                            })
+                        }
+
+                    </Swiper>
                 </div>
-                <Swiper
-                    slidesPerView={1}
-                    spaceBetween={1}
-                    navigation={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[Navigation, Pagination]}
-                    className="mySwiper swiperCustom"
-                >
-                    {
-                        sliderContent.slider.map((sliderItem) => {
-                            return (
-                                <SwiperSlide key={sliderItem.id} className="!flex items-start pl-[10px]"><div className="relative w-full">
-                                    <div className="border border-border absolute left-0 top-0 w-full h-full scale-[1.03] scale-y-[1.05]"></div>
-                                    {
-                                        console.log(sliderItem)
-                                    }
-                                    <Image src={sliderItem.thumbnail.data[0].attributes.url} width={13} layout="responsive" height={13} alt=""></Image> 
-
-                                </div></SwiperSlide>
-                            )
-                        })
-                    }
-
-                </Swiper>
             </div>
-        </div>
+        </>
     )
 }
