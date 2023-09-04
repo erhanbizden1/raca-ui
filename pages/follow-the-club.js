8801ff758cf676c16d3ba7b01ab390a86ffdc90a
Erhan Bizden <erhanb@dopinghafiza.com>
2023-09-04 09:47:31 +0300

    live-branch

--- pages/follow-the-club.js
+++ pages/follow-the-club.js
@@ -3,19 +3,22 @@ import { FacebookEmbed } from 'react-social-media-embed';
 import { InstagramEmbed } from 'react-social-media-embed';
 import { LinkedInEmbed } from 'react-social-media-embed';
 import BreadCrumb from '../components/BreadCrumb'
+import Head from "next/head";
 export default function FollowTheClub({ follow }) {
-  console.log(follow)
   return (
     <>
+      <Head>
+        <title>Royal Automobile Club of Australia - Follow The Club</title>
+      </Head>
       <div className="mb-[100px]">
         <BreadCrumb />
       </div>
       <div className="container pb-[100px]">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-[12px]">
-            
-            <InstagramEmbed url={follow?.instagram} style={{width:"100%"}} />
-            <FacebookEmbed url={follow?.facebook} style={{width:"100%"}} />
-            <LinkedInEmbed url={follow?.linkedin} style={{width:"100%"}} />
+
+          <InstagramEmbed url={follow?.instagram} style={{ width: "100%" }} />
+          <FacebookEmbed url={follow?.facebook} style={{ width: "100%" }} />
+          <LinkedInEmbed url={follow?.linkedin} style={{ width: "100%" }} />
         </div>
       </div>
     </>
